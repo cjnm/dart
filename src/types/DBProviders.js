5ce1b1bd504a430da2df8b4e805081dbc003cdd7
Seejan <seejan@pop-os.localdomain>
2022-04-21 00:15:08 +0545

    js removed

--- src/types/DBProviders.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var DBProviders;
-(function (DBProviders) {
-    DBProviders[DBProviders["mysql"] = 0] = "mysql";
-    DBProviders[DBProviders["supabase"] = 1] = "supabase";
-})(DBProviders || (DBProviders = {}));
-exports["default"] = DBProviders;
--- src/types/StorageProviders.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var StorageProviders;
-(function (StorageProviders) {
-    StorageProviders[StorageProviders["s3"] = 0] = "s3";
-    StorageProviders[StorageProviders["supabase"] = 1] = "supabase";
-})(StorageProviders || (StorageProviders = {}));
-exports["default"] = StorageProviders;
